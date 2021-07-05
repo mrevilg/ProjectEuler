f6f727182308f8f3b67008455bb3e2ead2d97059
Stephen Clements <mrevilg@gmail.com>
2021-07-05 11:03:45 -0500

    Final

--- Problem2.py
+++ Problem2.py
@@ -1,2 +1,16 @@
 # Even Fibonacci Numbers (each # is prior two #'s added)
 # 1, 2, 3, 5, 8, 13, 21, ...
+# Q: Find the sum of the even-valued terms, no term exceeding 4M
+
+x, y, z = 1, 1, 0
+result = 0
+
+while z < 4000000:
+    z = (x+y)
+    if z % 2 == 0:
+        result = result + z
+
+    x = y
+    y = z
+
+print(result)
