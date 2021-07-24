9e5bdb80e3ba21b0ebf14db85f6c87844dd5ded6
Stephen Clements <mrevilg@gmail.com>
2021-07-24 12:38:51 -0500

    Not completed

--- Problem3.py
+++ Problem3.py
@@ -3,3 +3,27 @@
 
 # Prime Factorization is finding which prime 
 # numbers X together to make the original number
+
+import math
+
+def isPrime(x):
+    if x<2:
+        return False
+    for i in range(2,int(math.sqrt(x))):
+        if not x%i:
+           return False
+    return True
+
+def largest_factor(number):
+    for i in range (2, int(math.sqrt(number))):
+        if number%i == 0:
+            number = number/i
+            if isPrime(number) is True:
+                max_val = number
+                break
+                print (max_val)
+        else:
+            i += 1
+    return max_val
+
+largest_factor(600851475143)
